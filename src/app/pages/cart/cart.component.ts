043d82fea9c88a7155668892337bdb60e940f4dc
Nthuyduong <nguyenthuyduong15113@gmail.com>
2023-06-24 08:37:52 +0700

    reload cart page

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,13 +1,26 @@
-import {Component} from "@angular/core";
-import {Data} from "../../interfaces/data.interface";
+import { Component, OnInit } from "@angular/core";
+import { Data } from "../../interfaces/data.interface";
+import { Router, NavigationStart } from '@angular/router';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
+export class CartComponent implements OnInit {
+  constructor(private router: Router) {}
 
-export class CartComponent {
+  ngOnInit() {
+    this.router.events
+        .pipe(filter((event) => event instanceof NavigationStart))
+        .subscribe(() => {
+          setTimeout(() => {
+            location.reload();
+          }, 1000); // Add a delay of 1 second (1000 milliseconds) before reloading the page
+        });
+  }
+
   quickviewed?: Data;
 
   quickview(item: Data) {
